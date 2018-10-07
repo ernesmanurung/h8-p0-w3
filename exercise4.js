94a9ece9d1c288bea5ff5903b2559955defde4b3
ernest <ernesmanurungg@gmail.com>
2018-10-08 00:41:11 +0700

    exercise

--- exercise4.js
+++ exercise4.js
@@ -11,30 +11,18 @@ function dataHandling2(input) {
     var bulan = split[1];
 
     switch (bulan) {
-        case '01': console.log("Januari");
-            break;
-        case '02': console.log("Februari");
-            break;
-        case '03': console.log("Maret");
-            break;
-        case '04': console.log("April");
-            break;
-        case '05': console.log("Mei");
-            break;
-        case '06': console.log("Juni");
-            break;
-        case '07': console.log("Juli");
-            break;
-        case '08': console.log("Agustus");
-            break;
-        case '09': console.log("September");
-            break;
-        case '10': console.log("Oktober");
-            break;
-        case '11': console.log("November");
-            break;
-        case '12': console.log("Desember");
-            break;
+        case '01': console.log("Januari"); break;
+        case '02': console.log("Februari"); break;
+        case '03': console.log("Maret"); break;
+        case '04': console.log("April"); break;
+        case '05': console.log("Mei"); break;
+        case '06': console.log("Juni"); break;
+        case '07': console.log("Juli"); break;
+        case '08': console.log("Agustus"); break;
+        case '09': console.log("September"); break;
+        case '10': console.log("Oktober"); break;
+        case '11': console.log("November"); break;
+        case '12': console.log("Desember"); break;
         default:
             break;
     }
